b3faee04a554489028e42290f5ce45b677eaba7b
Amr El Bakry <amr.bakry@yallawain.com>
2017-07-26 18:19:36 +0200

    Style about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,18 @@ var React = require('react');
 var About = (props) => {
   return(
     <div>
-      <h3>About component</h3>
-      <p>Welcome to About page.</p>
+      <h1 className='text-center'>About</h1>
+      <p>I have built this app for The Complete React Web App.</p>
+      <ul>
+        <li>
+          <a href='https://facebook.github.io/react'>React</a> - A JavaScript framework for building
+            user interfaces.
+        </li>
+        <li>
+          <a href='http://openweathermap.org'>Open Weather Map</a> - Weather data is provided by
+            OpenWeatherMap.
+        </li>
+      </ul>
     </div>
   );
 };
